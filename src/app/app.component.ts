3cc29c1931d27d47f2b289c52793741dd2047f1e
Dale Stone <dale@stonenet.co.uk>
2018-04-06 23:40:56 +0100

    Used svg pan zoom libraryu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import * as hammerjs from 'hammerjs';
+import * as svgPanZoom from 'svg-pan-zoom';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,6 @@ import * as hammerjs from 'hammerjs';
 })
 export class AppComponent {
   @ViewChild('svgContainer') svgContainer: ElementRef;
-  transform = {
-    translate: 'translate(0,0)',
-    scale: 'scale(1)',
-    set: (translate: string, scale: string) => {
-      this.transform.translate = translate;
-      this.transform.scale = scale;
-    },
-    get: () => {
-      return `${this.transform.translate} ${this.transform.scale}`
-    }    
-  };
 
   constructor(private httpClient: HttpClient) {
 
@@ -33,43 +23,38 @@ export class AppComponent {
   getSvg() {
     this.httpClient.get('/assets/usaTerritories2High.svg', {
       responseType: 'text'
-    }).subscribe((svg:string) => {
-      let parser = new DOMParser();      
+    }).subscribe((svg: string) => {
+      let parser = new DOMParser();
       let element = parser.parseFromString(svg, 'image/svg+xml');
       let svgElem = element.querySelector('svg');
-      let transformGrp = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-      transformGrp.innerHTML = svgElem.innerHTML;
-      transformGrp.setAttribute('id', 'pan');
-      transformGrp.setAttribute('transform', `${this.transform.translate} ${this.transform.scale}`);
 
+      this.svgContainer.nativeElement.innerHTML = svgElem.innerHTML;
+      // https://github.com/ariutta/svg-pan-zoom
+      let pan = svgPanZoom(this.svgContainer.nativeElement, {      
+        // onZoom: () => {
+        //   console.log('svg zooming...', pan.getZoom());                  
+        // },           
+        // customEventsHandler: {
+        //   haltEventListeners: [],
+        //   init: (options) => {
+        //     options.instance.zoomIn();
+        //   },
+        //   destroy: () => {
 
-      this.svgContainer.nativeElement.innerHTML = transformGrp.outerHTML;
-      this.setGestures(this.svgContainer.nativeElement);
+        //   }
+        // },
+        // controlIconsEnabled: false             
+      })
+
+      pan.setOnZoom(() => {
+        console.log('svg zooming...', pan.getZoom());                  
+      })
+
+      // let hammer = new Hammer(this.svgContainer.nativeElement);
+      // hammer.on('tap', () => {
+      //   console.log('svg tapped');
+      // })
     });
   }
 
-  setGestures(element: any) {
-    let hammer = new Hammer(element);
-    let g = document.getElementById('pan');
-
-    hammer.on('pan', (ev:any) => {
-      let pt = this.svgContainer.nativeElement.createSVGPoint();
-      pt.x = ev.srcEvent.clientX;
-      pt.y = ev.srcEvent.clientY;
-
-      let svgPt = pt.matrixTransform(this.svgContainer.nativeElement.getScreenCTM().inverse());
-
-      // translate.substring(translate.indexOf('(') + 1, translate.indexOf(')')).split(',').map(t => +t);
-      // [-337.32086181640625, -158.29409790039062]
-
-      let x = svgPt.x - ev.srcEvent.clientX;
-      let y = svgPt.y - ev.srcEvent.clientY;
-
-      this.setTransform(g, x, y, 1);      
-    });
-  }
-  
-  setTransform(element, x, y, scale) {
-    element.setAttribute('transform', `translate(${x},${y}) scale(${scale})`);
-  }    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
